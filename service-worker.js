579f8c2f5f2e961378ddd4969455a3df36f296ce
neelan-11 <neelan238@gmail.com>
2026-08-26 13:01:07 +0200

    Fix N2 offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,28 @@
-const CACHE = 'n2-pwa-v2-3';
+const CACHE = 'n2-pwa-v2-3-fixed';
 const CORE = [
   './',
   './index.html',
   './manifest.webmanifest',
   './icons/icon-180.png',
-  './icons/icon-512.png',
-  './icons/icon-1024.png'
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      const network = fetch(event.request)
-        .then(response => {
-          if (response && response.status === 200 && response.type !== 'opaque') {
-            const copy = response.clone();
-            caches.open(CACHE).then(cache => cache.put(event.request, copy));
-          }
-          return response;
-        })
-        .catch(() => cached || caches.match('./index.html'));
-
-      return cached || network;
-    })
-  );
+  event.respondWith(caches.match(event.request).then(cached => {
+    const network = fetch(event.request).then(response => {
+      if (response && response.status === 200 && response.type !== 'opaque') {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => cached || caches.match('./index.html'));
+    return cached || network;
+  }));
 });
